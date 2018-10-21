1347d7bc81f7106ff635d1734fd861c47e9d8f97
heverton <heverton.sarah@gmail.com>
2018-10-21 17:31:50 -0300

    Generate board peaces with its behaviors
    Peaces where created with list of behaviors so different ways of checking
    win condition can be made for each peace, according to its position on the
    board.
    
    See: #3

---
+++ Assets/Scripts/Test/BoardGenerationTest.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+namespace TicTacToe.Test
+{
+    public class BoardGenerationTest
+    {
+        /*
+        [Test]
+        public void NewPlayModeTestBoardGenerationTestSimplePasses()
+        {
+            // Use the Assert class to test conditions.
+        }
+
+        // A UnityTest behaves like a coroutine in PlayMode
+        // and allows you to yield null to skip a frame in EditMode
+        [UnityTest]
+        public IEnumerator NewPlayModeTestBoardGenerationTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // yield to skip a frame
+            yield return null;
+        }
+        */
+
+        [Test]
+        public void BoardGenerationTestPasses()
+        {
+            int boardSize = 3;
+            Board board = new Board(boardSize);
+            Assert.AreNotEqual(board.GetPeace(1, 1), null);
+        }
+
+        [Test]
+        public void PeacesBehaviorTestPasses()
+        {
+            int boardSize = 3;
+            Board board = new Board(boardSize);
+
+            Assert.AreEqual(board.GetPeace(0, 1).Behaviors.Count, 1);
+            Assert.AreEqual(board.GetPeace(1, 2).Behaviors.Count, 1);
+            Assert.AreEqual(board.GetPeace(2, 0).Behaviors.Count, 1);
+            Assert.AreEqual(board.GetPeace(1, 1).Behaviors.Count, 2);
+        }
+    }
+}
