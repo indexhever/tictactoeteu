a1dd0a6efdcf7f9e1ab4681aae9ac4633885db20
heverton <heverton.sarah@gmail.com>
2018-10-21 17:31:50 -0300

    Generate board peaces with its behaviors
    Peaces where created with list of behaviors so different ways of checking
    win condition can be made for each peace, according to its position on the
    board.
    
    See: #3

---
+++ Assets/Scripts/PeaceBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TicTacToe
+{
+    public class PeaceBehavior
+    {
+        public virtual bool CheckPeaceMatch(Board board)
+        {
+            return true;
+        }
+    }
+}
+
