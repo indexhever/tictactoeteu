2d02df00652ab0284dd633ac276eab0236ab31c7
heverton <heverton.sarah@gmail.com>
2018-10-25 06:34:53 -0300

    Handle peace visual and click event
    The Peaces did not have visual and click control. Event system was
    used to detect touch or click input on each peace.
    
    See: #7

---
+++ Assets/Scripts/PeaceComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace TicTacToe
+{
+    public class PeaceComponent : MonoBehaviour, IPointerClickHandler
+    {
+        private Peace peace;
+        [SerializeField]
+        private SpriteRenderer spriteRenderer;
+
+        // TODO: initialize icon container
+        public void Initialize(Peace peace)
+        {
+            this.peace = peace;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            //peace.Touch(GetCurrentPlayerIcon());
+            Debug.Log("Peace pressed");
+        }
+
+        // TODO: Get player icon from a player controller
+        private PeaceIcon GetCurrentPlayerIcon()
+        {
+            return PeaceIcon.None;
+        }
+
+        private void ChangeSpriteImage(Sprite newSprite)
+        {
+            spriteRenderer.sprite = newSprite;
+        }
+    }
+}
